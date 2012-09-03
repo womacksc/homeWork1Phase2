8df55882a5db9a84606d4460dee10d7a63012028
Harrison Bourne <bournehw@muohio.edu>
2012-09-03 15:03:25 -0400

    Basic Squares Implemented

--- src/homeWork1Phase2App.cpp
+++ src/homeWork1Phase2App.cpp
@@ -5,6 +5,7 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "Resources.h"
 #include <cmath>
+#include <vector>
 
 using namespace ci;
 using namespace ci::app;
@@ -21,6 +22,22 @@ class homeWork1Phase2App : public AppBasic {
 	private:
 		Surface* surface_;
 		int numFrames;
+
+		struct circle{
+			int x;
+			int y;
+			int r;
+		};
+
+		struct square{
+			int x;
+			int y;
+			int w;
+		};
+
+		deque<square> squares_;
+		deque<circle> circles_;
+
 	
 	//Width and height of the screen
 	static const int kAppWidth=800;
@@ -28,6 +45,7 @@ class homeWork1Phase2App : public AppBasic {
 	static const int kTextureSize=1024; //Must be the next power of 2 bigger or equal to app dimensions
 
 	void drawGradient(uint8_t* pixels, int shift);
+	void drawSquare(uint8_t* pixels, square s, Color8u c);
 
 };
 
@@ -40,12 +58,19 @@ void homeWork1Phase2App::setup()
 {
 		surface_ = new Surface(kTextureSize,kTextureSize,false);
 		numFrames = 0;
+		square test;
+		
+		for(int i=0; i<20; i++){
+			test.x = 20+i*10;
+			test.y = 40+i*10;
+			test.w = 20;
+			squares_.push_back(test); 
+		}
 }
 
 void homeWork1Phase2App::mouseDown( MouseEvent event )
 {	
-
-
+		
 }
 
 void homeWork1Phase2App::drawGradient(uint8_t* pixels, int shift){
@@ -71,11 +96,31 @@ void homeWork1Phase2App::drawGradient(uint8_t* pixels, int shift){
 	}
 }
 
+void homeWork1Phase2App::drawSquare(uint8_t* pixels, square s, Color8u c){
+	if(s.w<=0) return;
+
+	for(int y=s.y; y<=s.y+s.w; y++){
+		for(int x=s.x; x<=s.x+s.w; x++){
+			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
+
+			if(y==s.y||y==s.y+s.w||x==s.x||x==s.x+s.w){
+				pixels[3*(x + y*kTextureSize)] = c.r;
+				pixels[3*(x + y*kTextureSize)+1] = c.g;
+				pixels[3*(x + y*kTextureSize)+2] = c.b;
+			}
+		}
+	}
+}
+
 void homeWork1Phase2App::update()
 {
 	uint8_t* dataArray = (*surface_).getData();
 
 	drawGradient(dataArray, (numFrames*5)%kAppHeight);
+
+	for(int i=0; i<squares_.size(); i++){
+		drawSquare(dataArray, squares_[i], Color8u(0, 255, 0));
+	}
 
 	numFrames++;
 }
