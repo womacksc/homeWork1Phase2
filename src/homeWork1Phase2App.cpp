7d6b307c48e71634cd2efc305585e080d39bbd9b
Harrison Bourne <bournehw@muohio.edu>
2012-09-04 18:23:18 -0400

    Assignment Complete

--- src/homeWork1Phase2App.cpp
+++ src/homeWork1Phase2App.cpp
@@ -1,3 +1,14 @@
+/**
+* @file homeWork1Phase.cpp
+* CSE 274 - Fall 2012
+*
+* @author Harrison Bourne
+* @date 2012-09-04
+*
+* @note This project satisfies goals A.1 (rectangle), A.2 (circle), A.4 (gradient),
+* A.6 (tint) B.1 (blur), E.5 (animation) and E.6 (mouse interaction)
+*/
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
@@ -15,35 +26,60 @@ using namespace std;
 class homeWork1Phase2App : public AppBasic {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
     void prepareSettings(Settings* settings);
+	/**
+	 *Inside this function is the code wich creates the squares and circles
+	 *
+	 *This satisfies the "mouse interaction" requirement, goal E.6
+	 */
 	void mouseDrag( MouseEvent event );
+	/**
+	 *Inside this function is the code which resizes the shapes and which
+	 *changes the tint of the window.
+	 *
+	 *This satisfies the "tint" requirement, goal A.6
+	 */
 	void mouseWheel( MouseEvent event );
+	/**
+	 *Inside this function is the code which turns bluring  on and off.
+	 */
 	void keyDown( KeyEvent event );
 
 	private:
+		//Surface object pixel array
 		Surface* surface_;
-		int numFrames, drawSize_, mouseX_, mouseY_;
+		//Number of frames that have been shown (for animation)
+		int numFrames_;
+		//Size of shapes to draw.
+		int drawSize_;
+		//Keep track of mouse location
+		int mouseX_, mouseY_;
+		//Keep track of if blur is on or off.
 		bool blurFlag_;
 		Color8u squareColor_;
 		Color8u circleColor_;
+		//amount of green tint to use
 		uint8_t tintValue_;
 
+		//Structure to hold circle information
 		struct circle{
 			int x;
 			int y;
 			int r;
 		};
 
+		//Structure to hold square information
 		struct square{
 			int x;
 			int y;
 			int w;
 		};
 
+		//Keep track of squares drawn
 		deque<square> squares_;
+		//Keep track of circles drawn
 		deque<circle> circles_;
 
 	
@@ -52,9 +88,29 @@ class homeWork1Phase2App : public AppBasic {
 	static const int kAppHeight=600;
 	static const int kTextureSize=1024; //Must be the next power of 2 bigger or equal to app dimensions
 
+	/**
+	 *Draw a red and blue gradient with sifted by a given ammout.
+	 *
+	 *This satisfies the "gradient" requirment, goal A.4 and "animation", goal E.5
+	 */
 	void drawGradient(uint8_t* pixels, int shift);
+	/**
+	 *Draw a square with center and size given by s in the color given by c.
+	 *
+	 *This satisfies the "rectangle" requirement, goal A.1
+	 */
 	void drawSquare(uint8_t* pixels, square s, Color8u c);
-	void drawCircle(uint8_t* pixels, circle s, Color8u c);
+	/**
+	 *Draw a cricle with center and size given by cir in the color given by c.
+	 *
+	 *This satisfies the "circle" requirment, goal A.2
+	 */
+	void drawCircle(uint8_t* pixels, circle cir, Color8u c);
+	/**
+	 *Blurs the entire window
+	 *
+	 *This satisfies the "blur" requirement, goal B.1
+	 */
 	void blur(uint8_t* pixels);
 
 };
@@ -66,7 +122,7 @@ void homeWork1Phase2App::prepareSettings(Settings* settings){
 
 void homeWork1Phase2App::setup()
 {		surface_ = new Surface(kTextureSize,kTextureSize,false);
-		numFrames = 0;
+		numFrames_ = 0;
 		drawSize_ = 50;
 		srand ( time(NULL) );
 		squareColor_ = Color8u(255*rand(),255*rand(),255*rand()) ;
@@ -76,11 +132,6 @@ void homeWork1Phase2App::setup()
 		mouseX_ = -100; 
 		mouseY_ = -100;
 }		  
-
-void homeWork1Phase2App::mouseDown( MouseEvent event )
-{
-
-}
 
 void homeWork1Phase2App::mouseDrag( MouseEvent event )
 {	square s;
@@ -99,7 +150,7 @@ void homeWork1Phase2App::mouseDrag( MouseEvent event )
 		}
 		if(squares_.size()>knumShapes)
 			squares_.pop_front();
-	}
+}
 
 	if(event.isRightDown()){
 		cir.x = event.getX();
@@ -119,7 +170,6 @@ void homeWork1Phase2App::mouseWheel( MouseEvent event ){
 	float wheel = event.getWheelIncrement();
 	int temp;
 	
-
 	if(event.isShiftDown()){
 		temp = ((int)tintValue_)+((int)wheel)*5;
 		if((temp<255&&wheel>0)||(temp>0&&wheel<0))
@@ -129,14 +179,11 @@ void homeWork1Phase2App::mouseWheel( MouseEvent event ){
 	}
 	else
 		drawSize_ +=((int)wheel)*10;
-
-	
 }
 
 void homeWork1Phase2App::keyDown( KeyEvent event ) {
     if( event.getChar() == 'b' )
 		blurFlag_ = !blurFlag_;
-     
 }
 
 void homeWork1Phase2App::drawGradient(uint8_t* pixels, int shift){
@@ -236,7 +283,7 @@ void homeWork1Phase2App::update()
 {	
 	uint8_t* dataArray = (*surface_).getData();
 
-	drawGradient(dataArray, (numFrames*5)%kAppHeight);
+	drawGradient(dataArray, (numFrames_*5)%kAppHeight);
 
 	for(uint8_t i=0; i<squares_.size(); i++){
 		drawSquare(dataArray, squares_[i], squareColor_);
@@ -249,12 +296,11 @@ void homeWork1Phase2App::update()
 	if(blurFlag_)
 		blur(dataArray);
 
-	numFrames++;
+	numFrames_++;
 }
 
 void homeWork1Phase2App::draw()
 {
-	// clear out the window with black
 	gl::draw(*surface_);
 }
 
